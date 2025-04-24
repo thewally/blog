0fb77144661d8205091d72759384c05112b33e55
Arjen van der Wal <arjen.vanderwal@ns.nl>
2025-04-24 15:37:45 +0200

    Added meta details

--- src/app/[slug].tsx
+++ src/app/[slug].tsx
@@ -4,6 +4,7 @@ import { useLocalSearchParams } from "expo-router";
 import { getPost, getAllPosts } from "../repository/postRepository";
 import Markdown from "react-native-markdown-display";
 import { ScrollView } from "react-native-gesture-handler";
+import Head from "expo-router/head";
 
 export async function generateStaticParams(): Promise<Record<string, string>[]> {
     const posts = getAllPosts();
@@ -20,22 +21,28 @@ const PostDetailsPage = () => {
     }
 
     return (
-        <ScrollView
-            style={{
-                flex: 1,
-                backgroundColor: 'white',
-            }}
-            contentContainerStyle={{
-                maxWidth: 960,
-                width: '100%',
-                marginHorizontal: 'auto',
-                padding: 20,
-            }}
-        >
-            <Text style={{ fontSize: 30, marginBottom: 20 }}>{post.title}</Text>
+        <>
+            <Head>
+                <title>{post.title}</title>
+                <meta name="description" content={post.description} />
+            </Head>
+            <ScrollView
+                style={{
+                    flex: 1,
+                    backgroundColor: 'white',
+                }}
+                contentContainerStyle={{
+                    maxWidth: 960,
+                    width: '100%',
+                    marginHorizontal: 'auto',
+                    padding: 20,
+                }}
+            >
+                <Text style={{ fontSize: 30, marginBottom: 20 }}>{post.title}</Text>
 
-            <Markdown>{post.content}</Markdown>
-        </ScrollView>
+                <Markdown>{post.content}</Markdown>
+            </ScrollView>
+        </>
     );
 };
 
